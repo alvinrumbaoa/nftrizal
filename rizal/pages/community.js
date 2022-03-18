b74b7d35efbe850cfada8e478a0b265ec31075d5
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-03-17 23:18:41 -0700

    sent

---
+++ rizal/pages/community.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Flex, Heading,Text} from '@chakra-ui/react'
+
+const community = () => {
+	return (
+		<Flex d="flex" justify="space-around" align="center" direction="column" w={'full'} h={'100vh'} bgGradient='linear(to-b, #000000, #9F3AC3)'> 
+		<Heading color="white"> Thank you for joining. We will notify you from upcoming events for NFT Rizal</Heading>
+</Flex>
+	)
+}
+
+export default community
